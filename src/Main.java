a3f0d642ca9853925f7d0cfc74db0d372b214bce
lpycukl <44721183+lpycukl@users.noreply.github.com>
2023-12-14 11:10:02 +0600

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords() {
+        System.out.printf("%s %n%s %n%s %n", "Orange", "Banana", "Apple");
+    }
+
+    static void checkSumSign() {
+        int a = 3;
+        int b = 0;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    static void printColor() {
+        int value = 789;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (0 < value && value <= 100) {
+            System.out.println("Желтый");
+        } else System.out.println("Зеленый");
+    }
+
+    static void compareNumbers() {
+        int a = 3;
+        int b = 4;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }
